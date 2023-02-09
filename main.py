521391fe09561d0557892abe2c3ee282e5529cca
nabob <ignat.plehov@yandex.ru>
2023-02-09 17:49:17 +0500

    4 zadanie

--- main.py
+++ main.py
@@ -24,5 +24,38 @@ def img():
     <img src='{url_for('static', filename='img/MARS.png')}'>
     <p> Вот какая она красная планета</p>
     """
+
+@app.route("/promotion_image")
+def promote_img():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+                    <h1> Жди нас, Марс! </h1>          
+                    <img src='{url_for('static', filename='img/MARS.png')}'>
+                    <div class="alert alert-dark" role="alert">
+                        Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                        Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                        Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                        И начнем с Марса!
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                        Присоединяйся!
+                    </div>
+                  </body>
+                </html>
+    """
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
